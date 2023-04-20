6f5289682dea360b8e35ac854ae1aef4e69d709c
CampbellBiology <126536142+CampbellBiology@users.noreply.github.com>
2023-04-20 14:04:15 +0900

    Add files via upload

---
+++ MMN_test/src/main/webapp/resources/js/login.js
@@ -0,0 +1,41 @@
+function logID() {
+    let ID_txtbx = document.querySelector('#user_id');
+    let regID = /^[a-z0-9-_]{5,20}$/;
+
+    if (ID_txtbx.value == "") {
+        document.querySelector('#id_noti').innerHTML = "필수값입니다";
+    }
+    else if (!regID.test(ID_txtbx.value)) //정규식
+    {
+        document.querySelector('#id_noti').style.display = "block";
+        document.querySelector('#id_noti').innerHTML = "5~20자의 영문 소문자, 숫자와 특수기호(_),(-)만 사용 가능합니다.";
+    }
+    else { document.querySelector('#id_noti').style.display = "none"; }
+}
+
+function logPW() {
+    let PW_txtbx = document.querySelector('#user_pw');
+    let regPW = /^[a-z0-9-_]{5,20}$/;
+
+
+    if (PW_txtbx.value == "") {
+        document.querySelector('#pw_noti').innerHTML = "필수값입니다";
+    }
+    else if (!regPW.test(PW_txtbx.value)) //정규식
+    {
+        document.querySelector('#pw_noti').style.display = "block";
+        document.querySelector('#pw_noti').innerHTML = "8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.";
+    }
+    else {
+        document.querySelector('#pw_noti').style.display = "none";
+    }
+
+}
+
+
+function cancel_login() {
+    document.querySelector('#user_id').value = null;
+    document.querySelector('#user_pw').value = null;
+    document.querySelector('#id_noti').style.display = "none";
+    document.querySelector('#pw_noti').style.display = "none";
+}
