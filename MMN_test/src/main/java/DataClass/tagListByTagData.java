78f4b38c26e81b26d4593804095f75d4dd7c06b9
wjy0117 <76506444+wjy0117@users.noreply.github.com>
2023-04-21 15:28:17 +0900

    Update tagListByTagData.java

--- MMN_test/src/main/java/DataClass/tagListByTagData.java
+++ MMN_test/src/main/java/DataClass/tagListByTagData.java
@@ -1,9 +1,9 @@
 package DataClass;
 
 import java.util.ArrayList;
-
 import DB.DB_Conn;
 
+//메인 페이지에서 가게이미지 hover시에 사용하는 데이터를 리스트로 만든다.
 public class tagListByTagData {
 	ArrayList <tagListData> tldList;
 	
