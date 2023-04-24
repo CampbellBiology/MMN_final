1635c69e200e78ee333834ab99e7a43ceeede758
wjy0117 <76506444+wjy0117@users.noreply.github.com>
2023-04-24 14:34:54 +0900

    Add files via upload
    리뷰 정보 체크를 위한 서블릿 생성.

---
+++ MMN_test/src/main/java/InsertServlet/reviewSortServlet.java
@@ -0,0 +1,53 @@
+package InsertServlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class reviewSortServlet
+ */
+@WebServlet("/reviewSortServlet")
+public class reviewSortServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public reviewSortServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		request.setCharacterEncoding("utf-8");
+		// 현재 파일 경로 저장
+		String context = request.getContextPath();
+		
+		String review_SortTexted = request.getParameter("review_SortTexted");
+		//System.out.println(review_SortTexted);
+		
+		HttpSession session = request.getSession();
+		session.setAttribute("test", review_SortTexted);
+		
+		response.sendRedirect(context + "/Store.jsp");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
