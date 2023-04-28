dd963b5a2b185b98a1d3416dce4850e55325521b
CampbellBiology <126536142+CampbellBiology@users.noreply.github.com>
2023-04-28 10:22:43 +0900

    Add files via upload

--- MMN_test/src/main/webapp/resources/js/header.js
+++ MMN_test/src/main/webapp/resources/js/header.js
@@ -1,29 +1,5 @@
 
-	function sendRequest2() {
-		var httpRequest;
-		function createRequest() {
-			if (window.XMLHttpRequest) { // 익스플로러 7과 그 이상의 버전, 크롬, 파이어폭스, 사파리,
-											// 오페라 등
-				return new XMLHttpRequest();
-			} else { // 익스플로러 6과 그 이하의 버전
-				return new ActiveXObject("Microsoft.XMLHTTP");
-			}
-		}
-		function receiveResponse() {
-			// XMLHttpRequest 객체의 현재 상태가 요청 완료이고, 서버에 문서가 존재하면 받은 데이터를 출력함.
-			if (httpRequest.readyState == XMLHttpRequest.DONE
-					&& httpRequest.status == 200) {
-				document.getElementById("watchlist_popup").innerHTML = httpRequest.responseText;
-			}
-		}
-		httpRequest = createRequest(); // XMLHttpRequest 객체를 생성함.
-		httpRequest.onreadystatechange = receiveResponse; // XMLHttpRequest 객체의 현재
-															// 상태를 파악함.
-		// GET 방식의 비동기식 요청으로 Http 요청을 생성함.
-		httpRequest.open("POST", "watchlist_popup.jsp", true);
-		httpRequest.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-		httpRequest.send("userID=<%=userID%>"); // Http 요청을 보냄.
-		}
+	
 		
 		
 
@@ -51,3 +27,4 @@ function forPopup(storeCode){
 	
 //	doc1.href="../../Store.jsp?storeCode="+storeCode;
 }
+
