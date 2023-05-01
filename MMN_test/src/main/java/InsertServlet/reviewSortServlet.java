c299606a9a0fafeaaf1d542e8e46a162842efc07
wjy0117 <76506444+wjy0117@users.noreply.github.com>
2023-05-01 11:18:13 +0900

    Update reviewSortServlet.java

--- MMN_test/src/main/java/InsertServlet/reviewSortServlet.java
+++ MMN_test/src/main/java/InsertServlet/reviewSortServlet.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpSession;
 /**
  * Servlet implementation class reviewSortServlet
  */
-@WebServlet("/reviewSortServlet")
+@WebServlet("/resources/view_0427/reviewSortServlet")
 public class reviewSortServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -29,16 +29,18 @@ public class reviewSortServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		//response.getWriter().append("Served at: ").append(request.getContextPath());
-		request.setCharacterEncoding("utf-8");
-		// 현재 파일 경로 저장
+		request.setCharacterEncoding("utf-8"); 
+		//현재 파일 경로 저장
 		String context = request.getContextPath();
+		//원래 페이지로 이동을 위해
+		String review_store = request.getParameter("review_storeCode");
 		
+		//리뷰 정렬번호
 		String review_SortTexted = request.getParameter("review_SortTexted");
-		//System.out.println(review_SortTexted);
 		
+		//리뷰 정렬번호를 세션을 통해 전달
 		HttpSession session = request.getSession();
 		session.setAttribute("test", review_SortTexted);
-		
 		response.sendRedirect(context + "/resources/view_0427/Store_0427.jsp?storeCode=" + review_store);
 	}
 
