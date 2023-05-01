608a3ada45b247d135df05b73e03caa68b30f4c4
wjy0117 <76506444+wjy0117@users.noreply.github.com>
2023-05-01 10:52:12 +0900

    Update reviewServlet.java
    1. 이미지 저장경로 추가
    2. 저장하다 문제시 에러발생문 추가 후 원래 페이지로 이동

--- MMN_test/src/main/java/InsertServlet/reviewServlet.java
+++ MMN_test/src/main/java/InsertServlet/reviewServlet.java
@@ -1,18 +1,24 @@
 package InsertServlet;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.oreilly.servlet.MultipartRequest;
+
 import DataClass.*;
 import DB.*;
 
 /**
  * Servlet implementation class reviewServlet
  */
-@WebServlet("/review")
+@WebServlet("/resources/view_0427/review")
 public class reviewServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
@@ -32,7 +38,7 @@ public class reviewServlet extends HttpServlet {
 			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		// response.getWriter().append("Served at: ").append(request.getContextPath());
-		// System.out.println("리뷰 서블릿 호출");
+		System.out.println("리뷰 서블릿 호출");
 		// 인코딩 지정
 		request.setCharacterEncoding("utf-8");
 		// 현재 파일 경로 저장
@@ -43,33 +49,64 @@ public class reviewServlet extends HttpServlet {
 
 		// 리뷰데이터 모델클래스.
 		reviewData sd = new reviewData();
-
+		String  review_imgurls = "";
+		String review_store = "";
 		// 리뷰페이지에서 리뷰 내용을 가져와 DB에 입력
 		// 각 페이지가 아직 연결되지 않아 세션가져오기 어려움
 		// 임시로 아이디 입력칸 생성
 		try {
+			String path = "C:/Apache24/htdocs/ImageTest/storeImgSub";
+			File f = new File(path);
+	    	//파일이 있는지 체크
+	    	if(!f.exists()){
+	    		//파일폴더 생성.
+	            f.mkdirs();
+	        }
+			MultipartRequest multi = new MultipartRequest(request, path, 1024 * 1024 * 10, "utf-8");
 			// 메뉴 리스트와 태그 리스트.
-			String menuListView = request.getParameter("menuListView");
-			String tagListView = request.getParameter("tagListView");
+			String menuListView = multi.getParameter("menuListView");
+			String tagListView = multi.getParameter("tagListView");
 
 			// input 받아옴.
-			String review_noname = request.getParameter("noname_check"); // null인가 아닌가.
-			String review_store = request.getParameter("review_storeCode"); // request.getParameter("review_storeCode");
-			String review_id = request.getParameter("review_input_id");
-			String review_text = request.getParameter("review_text");
-			String review_score = request.getParameter("score_result"); // request.getParameter("score_review");
-
-			/*
-			 * //받아온 결과 출력. System.out.println("menu: "+menuListView);
-			 * System.out.println("tag: "+tagListView);
-			 * 
-			 * System.out.println("store: "+review_store);
-			 * System.out.println("id: "+review_id);
-			 * System.out.println("text: "+review_text);
-			 * System.out.println("score: "+review_score); //true: value, false: null(익명체크)
-			 * System.out.println("no: "+review_noname);
-			 */
-
+			String review_noname = multi.getParameter("noname_check"); // null인가 아닌가.
+			review_store = multi.getParameter("review_storeCode"); // request.getParameter("review_storeCode");
+			String review_id = multi.getParameter("review_input_id");
+			String review_text = multi.getParameter("review_text");
+			String review_score = multi.getParameter("score_result"); // request.getParameter("score_review");
+			
+			//이미지 input
+			String review_imgtext = multi.getParameter("name_imgtext");
+			String imgstr[] = review_imgtext.split(",");
+	        for(int i=1; i<imgstr.length; i++) {
+	        	
+	        	//이미지 파일명 변경.
+	        	File fileimg = new File("/ImageTest/storeImgSub/"+imgstr[i]);
+	        	File fileimgname = new File("/ImageTest/storeImgSub/"+review_store+"-"+review_id+"-"+i+".jpg");
+	        	fileimg.renameTo(fileimgname);
+	        	
+	        	//이미지 링크를 문자열로 만들기(DB입력용)
+	        	if(i==1) {
+	        		review_imgurls = '"'+fileimgname.toString()+'"';
+	        	}else {
+	        		review_imgurls = review_imgurls+","+'"'+fileimgname.toString()+'"';
+	        	}
+	        }
+	        //db에 저장하는 형식에 맞춰 url로 방식으로 변환
+	        review_imgurls = review_imgurls.replace("\\", "/");
+	        System.out.println("전체 이미지 경로: "+review_imgurls);
+			
+	        /*
+			//받아온 결과 출력.
+			System.out.println("menu: "+menuListView);
+			System.out.println("tag: "+tagListView);
+			
+			System.out.println("store: "+review_store);
+			System.out.println("id: "+review_id);
+			System.out.println("text: "+review_text);
+			System.out.println("score: "+review_score); //true: value, false: null(익명체크)
+			System.out.println("no: "+review_noname);
+			*/
+			
 			// 데이터 reviewData 방식으로 저장
 			sd.setStoreCode(review_store);
 			sd.setUserId(review_id);
@@ -82,11 +119,17 @@ public class reviewServlet extends HttpServlet {
 			dao.Insert_ReviewData(sd);
 			dao.Insert_List(menuListView, review_id, tagListView, review_store);
 			System.out.println("db 등록 끝");
-
-			response.sendRedirect(context + "/Store.jsp");
+			
 		} catch (Exception e) {
+			response.setContentType("text/html; charset=UTF-8");
+			PrintWriter writer = response.getWriter();
+			writer.println("<script>alert('문제가 발생하였습니다.'); location.href='"+context + "/Store.jsp"+"';</script>"); 
+			writer.close();
 			e.printStackTrace();
 		}
+		//"Store_0424.jsp?storeCode=<%=tlbt.getTldList().get(j).getStoreCode()%>"
+		//response.sendRedirect(context + "/View/Store_0424.jsp?storeCode="+review_store);
+		response.sendRedirect(context + "/resources/view_0427/Store_0427.jsp?storeCode=" + review_store);
 	}
 
 	/**
@@ -98,5 +141,4 @@ public class reviewServlet extends HttpServlet {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
-
 }
